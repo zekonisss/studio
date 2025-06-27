21198ee4dadfa945987235b307342bc6006f3a9d
Saras Zakas <sarunas.zekonis@gmail.com>
2025-06-27 15:55:10 +0000

    ERROR: build step 3 "gcr.io/k8s-skaffold/pack" failed: step exited with

---
+++ src/next.config.ts
@@ -0,0 +1,24 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
