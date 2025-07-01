95e4aeefadbe1be27f1a2d31e66e6d9cd528684f
Saras Zakas <sarunas.zekonis@gmail.com>
2025-07-01 05:49:01 +0000

    ERROR: build step 3 "gcr.io/k8s-skaffold/pack" failed: step exited with

--- next.config.ts
+++ next.config.ts
@@ -19,11 +19,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  devIndicators: {
-    allowedDevOrigins: [
-      "https://6000-firebase-studio-1747996775311.cluster-3gc7bglotjgwuxlqpiut7yyqt4.cloudworkstations.dev",
-    ],
-  },
 };
 
 export default nextConfig;
