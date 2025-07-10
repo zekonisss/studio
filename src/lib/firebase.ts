10f7480d51328df94c9c80ad57875d77e21de61b
Saras Zakas <sarunas.zekonis@gmail.com>
2025-07-10 13:43:33 +0000

    vistiek neduoda

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,15 +3,23 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// NOTE: This object is now populated with the correct Firebase configuration
-// from your project settings.
+// =================================================================
+// TODO: PASTE YOUR FIREBASE CONFIGURATION OBJECT HERE
+//
+// 1. Open your Firebase project settings.
+// 2. In the "General" tab, scroll down to "Your apps".
+// 3. Click the "</>" (Web) icon to see your app's config.
+// 4. Copy the `firebaseConfig` object and paste it below,
+//    replacing the entire FIREBASE_CLIENT_CONFIG object.
+// =================================================================
 const FIREBASE_CLIENT_CONFIG = {
-  apiKey: "AIzaSyBusklRtrpm-gfnwCdmi2yj5vTumqLte3c",
-  authDomain: "drivershield.firebaseapp.com",
-  projectId: "drivershield",
-  storageBucket: "drivershield.appspot.com",
-  messagingSenderId: "688007961476",
-  appId: "1:688007961476:web:d6d663ef7430182d781bd1"
+  // ===> PASTE YOUR REAL CONFIGURATION OBJECT HERE <===
+  apiKey: "REPLACE_WITH_YOUR_API_KEY",
+  authDomain: "REPLACE_WITH_YOUR_AUTH_DOMAIN",
+  projectId: "REPLACE_WITH_YOUR_PROJECT_ID",
+  storageBucket: "REPLACE_WITH_YOUR_STORAGE_BUCKET",
+  messagingSenderId: "REPLACE_WITH_YOUR_MESSAGING_SENDER_ID",
+  appId: "REPLACE_WITH_YOUR_APP_ID"
 };
 
 
