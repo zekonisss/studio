fa96bedc5e42367898951eb050bdde620d50ecd1
Saras Zakas <sarunas.zekonis@gmail.com>
2025-07-10 07:18:56 +0000

    ar galiu buti kad API raktas yra neteisingas ?

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,7 +2,18 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { FIREBASE_CLIENT_CONFIG } from './firebase-client-config';
+
+// NOTE: This object will be populated with the correct Firebase configuration
+// by the Firebase Studio environment during the build process.
+// Do not manually enter credentials here.
+const FIREBASE_CLIENT_CONFIG = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+};
 
 
 // Initialize Firebase
