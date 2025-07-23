3ac960295a5b9485c5bf79d397115bbfb40f64ed
Saras Zakas <sarunas.zekonis@gmail.com>
2025-07-23 09:57:02 +0000

    just to make sure firebase uses this code too Project settings General

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,6 @@
 
+"use client";
+
 // This file is safe to be imported on the client (browser).
 import { initializeApp, getApps, getApp, type FirebaseOptions } from "firebase/app";
 import { getFirestore, enableNetwork, Timestamp, serverTimestamp } from "firebase/firestore";
@@ -9,7 +11,7 @@ const FIREBASE_CLIENT_CONFIG: FirebaseOptions = {
   apiKey: "AIzaSyBusklRtrpm-gfnwCdmi2yj5vTumqLte3c",
   authDomain: "drivershield.firebaseapp.com",
   projectId: "drivershield",
-  storageBucket: "drivershield.appspot.com",
+  storageBucket: "drivershield.firebasestorage.app",
   messagingSenderId: "688007961476",
   appId: "1:688007961476:web:d6d663ef7430182d781bd1",
   measurementId: "G-WWHPWT8FGC"
