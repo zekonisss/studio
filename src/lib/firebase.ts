160103ffa4a4f7502697bccd476695779331e234
Saras Zakas <sarunas.zekonis@gmail.com>
2025-08-14 08:31:13 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBuskIRtmp-gfnwCdmi2yj5vTumqLte3c",
-  authDomain: "drivershield.firebaseapp.com",
-  projectId: "drivershield",
-  storageBucket: "drivershield.appspot.com",
-  messagingSenderId: "688007961476",
-  appId: "1:688007961476:web:d6d663ef7430182d781bd1",
-  measurementId: "G-WWHPWT8FGC"
+  "projectId": "drivershield",
+  "appId": "1:688007961476:web:0bf3aa76adba59f7781bd1",
+  "storageBucket": "drivershield.appspot.com",
+  "apiKey": "AIzaSyBusklRtrpm-gfnwCdmi2yj5vTumqLte3c",
+  "authDomain": "drivershield.firebaseapp.com",
+  "measurementId": "G-BKJYEF2X6Y",
+  "messagingSenderId": "688007961476"
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
