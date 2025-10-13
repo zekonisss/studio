697f9c3ba05a9a6337e93864018bdad368d0498e
Saras Zakas <sarunas.zekonis@gmail.com>
2025-10-13 14:45:51 +0000

    toki ismeta

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBusklRtrpm-gfnwCdmi2yj5vTumqLte3c",
+  authDomain: "drivershield.firebaseapp.com",
+  projectId: "drivershield",
+  storageBucket: "drivershield.appspot.com",
+  messagingSenderId: "688007961476",
+  appId: "1:688007961476:web:d6d663ef7430182d781bd1"
 };
 
 // Initialize Firebase
