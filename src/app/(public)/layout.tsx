bba5f3e1e112a74797742ba98d9a3a1767b270d4
Saras Zakas <sarunas.zekonis@gmail.com>
2025-11-24 20:25:35 +0000

    padaryk sita logotipa kaip migtuka, kuris grazintu i registracijos prisi

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,4 +1,5 @@
 import { UserSearch } from "lucide-react";
+import Link from "next/link";
 
 export default function UnauthenticatedLayout({
     children,
@@ -7,10 +8,10 @@ export default function UnauthenticatedLayout({
 }) {
     return (
         <div className="flex min-h-screen w-full items-center justify-center bg-muted/40 p-4">
-            <div className="absolute top-8 flex items-center gap-2 text-lg font-semibold text-primary">
+            <Link href="/login" className="absolute top-8 flex items-center gap-2 text-lg font-semibold text-primary cursor-pointer">
                 <UserSearch className="h-7 w-7" />
                 <span>DriverCheck</span>
-            </div>
+            </Link>
             {children}
         </div>
     );
