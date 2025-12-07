05952d456fdf9ca704db0464cbd313f7a7742e03
Saras Zakas <sarunas.zekonis@gmail.com>
2025-12-07 21:34:51 +0000

    ka padariai, kad negaliu prisijungti ? grazink atgal

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,7 +9,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBusklRtrpm-gfnwCdmi2yj5vTumqLte3c",
   authDomain: "drivershield.firebaseapp.com",
   projectId: "drivershield",
-  storageBucket: "drivershield.firebasestorage.app",
+  storageBucket: "drivershield.appspot.com",
   messagingSenderId: "688007961476",
   appId: "1:688007961476:web:0bf3aa76adba59f7781bd1",
   measurementId: "G-BKJYEF2X6Y"
