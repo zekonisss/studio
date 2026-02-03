f88fa1644f7a58cc53f2320017758d38545d4149
Saras Zakas <sarunas.zekonis@gmail.com>
2026-02-03 11:36:11 +0000

    ok

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-// Force cache refresh for ChunkLoadError - attempt 2.
+// Force cache refresh for ChunkLoadError - attempt 3.
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
