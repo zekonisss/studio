2c12b79ba79013b2c9522bfbc0db12fd9dbca532
Saras Zakas <sarunas.zekonis@gmail.com>
2026-02-12 13:58:03 +0000

    ChunkLoadError
    Warning: An error occurred during hydration. The server

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 
-// Force cache refresh for ChunkLoadError - attempt 6.
+// Force cache refresh for ChunkLoadError - attempt 7.
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   generateBuildId: async () => {
